e7d745a03e1a99a3d6736873ffc2fd879ef21d1e
t.kojima <t.kojima>
2019-05-16 22:41:49 +0900

    models.py修正

--- analysis/models.py
+++ analysis/models.py
@@ -11,13 +11,13 @@ class Group(models.Model):
 class User(models.Model):
     user_cd = models.CharField(primary_key=True, max_length=6, null=False)
     group_cd = models.CharField(max_length=2, null=False)
-    user_id = models.CharField(max_length=60, null=False)
+    user_id = models.CharField(unique=True, max_length=60, null=False)
     password = models.CharField(max_length=20, null=False)
     user_nm = models.CharField(max_length=60, null=False)
     mailto = models.CharField(max_length=60)
     authority_cd = models.CharField(max_length=2, null=False)
     use_flg = models.CharField(max_length=2, null=False)
-    created_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(default=timezone.now)
     update_date = models.DateTimeField(default=timezone.now)
 
 
@@ -33,14 +33,14 @@ class Question(models.Model):
     q_details_5 = models.CharField(max_length=60)
     sort_cd = models.CharField(max_length=6, null=False)
     use_flg = models.CharField(max_length=2, null=False)
-    created_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(default=timezone.now)
     update_date = models.DateTimeField(default=timezone.now)
 
 class Answer(models.Model):
-    user_cd = models.CharField(primary_key=True, max_length=6, null=False)
-    group_cd = models.CharField(primary_key=True, max_length=2, null=False)
-    question_cd = models.CharField(primary_key=True, max_length=6, null=False)
+    answer_cd = models.CharField(primary_key=True, max_length=6, null=False)
+    user_cd = models.CharField(max_length=6, null=False)
+    question_cd = models.CharField(max_length=6, null=False)
     answer = models.CharField(max_length=6)
 
-    def __str__(self):
-        return self.group_cd
+    class Meta:
+        unique_together = ('user_cd','question_cd')
