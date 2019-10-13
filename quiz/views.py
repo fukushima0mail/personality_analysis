a2474cdb26f50b36e99b1ea19942851fbeeb5e1f
t.kojima <workaddress0001@gmail.com>
2019-10-13 13:25:49 +0900

    fix view

--- quiz/views.py
+++ quiz/views.py
@@ -1,48 +1,45 @@
-from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.exceptions import NotFound
-from .models import Question, Group, User, Answer
-from .serializers import QuestionSerializer, UserSerializer, AnswerSerializer, GroupSerializer, \
-    GetUserAnswerValidateSerializer, RegistUserAnswerValidateSerializer, GetQuestionValidateSerializer, \
-    RegistGroupValidateSerializer, RegistUserValidateSerializer
-from django.http import JsonResponse, HttpResponse
-from django.db import IntegrityError
-import uuid
+from rest_framework.exceptions import NotFound, server_error
+from .models import Group, User, Answer
+from .serializers import GetUserAnswerValidateSerializer, RegistUserAnswerValidateSerializer,\
+    GetQuestionValidateSerializer, RegistGroupValidateSerializer, RegistUserValidateSerializer
+from django.http import HttpResponse
 import json
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    グループビューセット
-    """
-
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    ユーザビューセット
-    """
-
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    filter_fields = ('group',)
-
-
-class QuestionViewSet(viewsets.ModelViewSet):
-    """
-    質問ビューセット
-    """
-
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
-    filter_fields = ('group',)
+# class GroupViewSet(viewsets.ModelViewSet):
+#     """
+#     グループビューセット
+#     """
+#
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
+#
+#
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     ユーザビューセット
+#     """
+#
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     filter_fields = ('group',)
+#
+#
+# class QuestionViewSet(viewsets.ModelViewSet):
+#     """
+#     質問ビューセット
+#     """
+#
+#     queryset = Question.objects.all()
+#     serializer_class = QuestionSerializer
+#     filter_fields = ('group',)
 
 
 class GroupView(APIView):
     """/group"""
+
     def get(self, request):
         """グループ取得"""
         res = Group.objects.filter(is_deleted=False).values('group_id', 'group_name')
@@ -90,7 +87,7 @@ class SelectUserView(APIView):
         try:
             val = User.objects.get(user_id=user_id, is_deleted=False)
         except User.DoesNotExist:
-            raise Exception('対象のレコードが存在しません。')
+            raise NotFound()
 
         res = dict()
         res['user_id'] = val.user_id
@@ -113,7 +110,7 @@ class SelectUserAnswerView(APIView):
         try:
             res = Answer.objects.get(**data)
         except Answer.DoesNotExist:
-            raise Exception('対象のレコードが存在しません。')
+            raise NotFound()
         return HttpResponse(res)
 
     def post(self, request, user_id):
@@ -123,14 +120,15 @@ class SelectUserAnswerView(APIView):
         data.is_valid(raise_exception=True)
 
         try:
-            res = Answer.objects.create(*data.validated_data, user_id=user_id)
+            Answer.objects.create(*data.validated_data, user_id=user_id)
         except Exception as e:
-            raise e
-        return HttpResponse(res)
+            server_error()
+        return HttpResponse(status=204)
 
 
 class QuestionView(APIView):
     """/questions"""
+
     def get(self, request):
         """問題取得"""
         group_id = request.GET.get('group_id')
@@ -138,7 +136,15 @@ class QuestionView(APIView):
 
         data = GetQuestionValidateSerializer(data=dict(group_id=group_id, limit=limit))
         data.is_valid(raise_exception=TabError)
-        res = Answer.objects.filter(group_id=data.validated_data['group_id'], answer_id__in=list(1, 2, 3))
+        res = Answer.objects.filter(group_id=data.validated_data['group_id'], answer_id__in=list(1, 2, 3)).values(
+            'question_id', 'group_id', 'user_id', 'question_type', 'question',
+            'shape_path', 'correct', 'choice_1', 'choice_2', 'choice_3', 'choice_4'
+        )
+
+        if not res.exists():
+            raise NotFound()
+
+        return Response(res)
 
     def post(self, request, user_id):
         """問題登録(未作成)"""
@@ -148,6 +154,7 @@ class QuestionView(APIView):
 
 class SpecifiedQuestionView(APIView):
     """/questions/{question_id}"""
+
     def get(self, request, user_id):
         # Todo: 問題更新API
         pass
@@ -155,41 +162,3 @@ class SpecifiedQuestionView(APIView):
     def post(self, request, user_id):
         # Todo: 問題削除API
         pass
-
-class AnswerView(APIView):
-
-    def get(self, request, user_cd):
-        param = Answer.objects.filter(user=user_cd).values()
-        return Response(param)
-
-    def post(self, request, user_cd):
-        data = json.loads(request.body)
-
-        for val in data:
-            serializer = AnswerSerializer(data=val)
-            serializer.is_valid(raise_exception=True)
-
-            try:
-                item, create = Answer.objects.get_or_create(user_id=val["user_id"],
-                                                            question_id=val["question_id"],
-                                                            defaults=dict(
-                                                                answer=val["answer"]
-                                                            ))
-
-            except IntegrityError:
-                # 複合ユニークキーが登録済みの場合は更新
-                target = Answer.objects.filter(user_id=val["user_id"],
-                                               question_id=val["question_id"])
-                target.update(answer=val["answer"])
-
-        return HttpResponse(status=200)
-
-    def put(self, request, user_cd):
-        data = json.loads(request.body)
-        serializer = AnswerSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-
-        param = dict(user_id=data["user_id"], question_id=data["question_id"])
-        target = Answer.objects.filter(**param)
-        target.update(answer=data["answer"])
-        return HttpResponse(status=200)
